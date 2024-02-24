22e3685ae2659c6b9d5daebb742b9b37dd24d956
Everton-Gon <evertonkmlly16@gmail.com>
2024-02-24 10:53:18 -0300

    Conexao com banco

---
+++ Desafio_S4E/DAO/Conexao.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Desafio_S4E.DAO
+{
+    public class Conexao
+    {
+        public DataView Execute(string sql)
+        {
+            string connString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand cmd = new SqlCommand(sql, conn);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataSet dataSet = new DataSet();
+
+            try
+            {
+                conn.Open();
+                adapter.Fill(dataSet);
+                return dataSet.Tables[0].DefaultView;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public void ExecuteReader(string sql)
+        {
+            string connString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connString);
+            SqlCommand cmd = new SqlCommand(sql, conn);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+    }
+}
