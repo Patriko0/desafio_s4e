0cdba53ac0cef256bd5b403b2aae80118b6a83d8
Everton-Gon <evertonkmlly16@gmail.com>
2024-02-24 12:22:21 -0300

    Conexão etapa

---
+++ Desafio_S4E/DAO/EtapaDAO.cs
@@ -0,0 +1,55 @@
+﻿using Desafio_S4E.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Desafio_S4E.DAO
+{
+    public class EtapaDAO
+    {
+        public void Create(EtapaModel etapa)
+        {
+            try
+            {
+                string sql = $"INSERT INTO Etapa (descricao, dataInicio, dataConclusao) VALUES ('{etapa.Desc},{etapa.DataInicio},{etapa.DataConclusao}')";
+                Conexao conn = new Conexao();
+                conn.ExecuteReader(sql);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public DataView Read()
+        {
+            try
+            {
+                string sql = $"SELECT * FROM Etapa";
+                Conexao conn = new Conexao();
+                DataView data = conn.Execute(sql);
+                return data;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            try
+            {
+                string sql = $"DELETE FROM Etapa WHERE id = {id}";
+                Conexao conn = new Conexao();
+                conn.ExecuteReader(sql);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Desafio_S4E/Services/ServiceEtapa.cs
@@ -0,0 +1,55 @@
+﻿using Desafio_S4E.DAO;
+using Desafio_S4E.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Desafio_S4E.Services
+{
+    public class ServiceEtapa
+    {
+        public bool Store(EtapaModel etapa)
+        {
+            try
+            {
+                EtapaDAO etapaDAO = new EtapaDAO();
+                etapaDAO.Create(etapa);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public DataView Show()
+        {
+            try
+            {
+                EtapaDAO etapaDAO = new EtapaDAO();
+
+                return etapaDAO.Read();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            try
+            {
+                EtapaDAO etapaDAO = new EtapaDAO();
+                etapaDAO.Delete(id);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
