b852119c46e580ec121986c5744c2cdcb265a4b6
Everton-Gon <evertonkmlly16@gmail.com>
2024-02-24 12:21:36 -0300

    Conexão Tarefa

---
+++ Desafio_S4E/DAO/TarefaDAO.cs
@@ -0,0 +1,55 @@
+﻿using Desafio_S4E.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Desafio_S4E.DAO
+{
+    public class TarefaDAO
+    {
+        public void Create(TarefaModel tarefa)
+        {
+            try
+            {
+                string sql = $"INSERT INTO Tarefa (descricao, dataInicio) VALUES ('{tarefa.Desc},{tarefa.DataInicio}')";
+                Conexao conn = new Conexao();
+                conn.ExecuteReader(sql);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public DataView Read()
+        {
+            try
+            {
+                string sql = $"SELECT * FROM Tarefa";
+                Conexao conn = new Conexao();
+                DataView data = conn.Execute(sql);
+                return data;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            try
+            {
+                string sql = $"DELETE FROM Tarefa WHERE id = {id}";
+                Conexao conn = new Conexao();
+                conn.ExecuteReader(sql);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Desafio_S4E/Services/ServiceTarefa.cs
@@ -0,0 +1,55 @@
+﻿using Desafio_S4E.DAO;
+using Desafio_S4E.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Desafio_S4E.Services
+{
+    public class ServiceTarefa
+    {
+        public bool Store(TarefaModel tarefa)
+        {
+            try
+            {
+                TarefaDAO tarefaDAO = new TarefaDAO();
+                tarefaDAO.Create(tarefa);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public DataView Show()
+        {
+            try
+            {
+                TarefaDAO tarefaDAO = new TarefaDAO();
+
+                return tarefaDAO.Read();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            try
+            {
+                TarefaDAO tarefaDAO = new TarefaDAO();
+                tarefaDAO.Delete(id);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
