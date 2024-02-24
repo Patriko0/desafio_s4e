61715b6283f51a817a23b000f546afea35bf499f
Everton-Gon <evertonkmlly16@gmail.com>
2024-02-24 11:54:17 -0300

    Conexao com User

---
+++ Desafio_S4E/DAO/UserDAO.cs
@@ -0,0 +1,55 @@
+﻿using Desafio_S4E.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Desafio_S4E.DAO
+{
+    public class UserDAO
+    {
+        public void Create(UserModel user)
+        {
+            try
+            {
+                string sql = $"INSERT INTO Usuario (nome) VALUES ('{user.Name}')";
+                Conexao conn = new Conexao();
+                conn.ExecuteReader(sql);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public DataView Read()
+        {
+            try
+            {
+                string sql = $"SELECT * FROM Usuario";
+                Conexao conn = new Conexao();
+                DataView data = conn.Execute(sql);
+                return data;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            try
+            {
+                string sql = $"DELETE FROM Usuario WHERE id = {id}";
+                Conexao conn = new Conexao();
+                conn.ExecuteReader(sql);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ Desafio_S4E/Services/ServiceUser.cs
@@ -0,0 +1,55 @@
+﻿using Desafio_S4E.DAO;
+using Desafio_S4E.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Desafio_S4E.Services
+{
+    public class ServiceUser
+    {
+        public bool Store(UserModel user)
+        {
+            try
+            {
+                UserDAO userDao = new UserDAO();
+                userDao.Create(user);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public DataView Show()
+        {
+            try
+            {
+                UserDAO userDAO = new UserDAO();
+
+                return userDAO.Read();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            try
+            {
+                UserDAO userDAO = new UserDAO();
+                userDAO.Delete(id);
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
